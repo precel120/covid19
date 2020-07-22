69cd462cfc5fac11117af24fcea3f48599b1017e
precel120 <darkuso@o2.pl>
2020-07-22 18:08:14 +0200

    API connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
-const App = () => (
-  <h1>Hello World</h1>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      global: {},
+      countries: [],
+      date: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://api.covid19api.com/summary').then(res => {
+      if (res.status === 200) {
+        this.setState(() => ({
+          global: { ...res.data.Global },
+          countries: [...res.data.Countries],
+          date: res.data.Date,
+        }));
+      }
+    });
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
 
 export default App;
